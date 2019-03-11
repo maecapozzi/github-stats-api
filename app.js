7dc047dc2769d48aa1fb74a0bed8ea19af9e0ba2
maecapozzi <maecapozzi@gmail.com>
2019-03-11 15:59:58 -0400

    Working redirect

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
-var createError = require("http-errors");
-var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passport = require("passport");
+const indexRouter = require("./routes/index");
+const loginRouter = require("./routes/login");
+const GitHubStrategy = require("passport-github2").Strategy;
+
 require("dotenv").config();
 
-var app = express();
+const app = express();
 app.use(cors());
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const redis = require("redis");
+const client = redis.createClient();
+
+client.on("connect", function() {
+  console.log("Redis client connected");
+});
+
+client.on("error", function(err) {
+  console.log("Something went wrong " + err);
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:3001/auth/github/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      process.nextTick(function() {
+        // To keep the example simple, the user's GitHub profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the GitHub account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,9 +61,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(require("serve-static")(__dirname + "/../../public"));
+app.use(require("cookie-parser")());
+app.use(require("body-parser").urlencoded({ extended: true }));
+
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// app.use("/login", loginRouter);
+
+app.get("/login", function(req, res) {
+  res.render("login", { user: req.user });
+});
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  function(req, res) {
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  }
+);
+
+// GET /auth/github/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function(req, res) {
+    res.redirect("/");
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,6 +118,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 module.exports = app;
